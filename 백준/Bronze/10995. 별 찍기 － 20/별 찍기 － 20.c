01993cf357905468d678b93c6e0393641afbdf95
jo-eul <163392700+jo-eul@users.noreply.github.com>
2024-05-29 15:31:45 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			if (i % 2 == 0) {
+				printf("* ");
+			}
+			else {
+				printf(" *");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
