88260caa95f95f886d476fc876a4ad48074e0f1c
jo-eul <163392700+jo-eul@users.noreply.github.com>
2024-05-29 16:38:11 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 352 ms, Memory: 18436 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        int a=scanner.nextInt();
+
+        for(int i=1; i<=a; i++){
+            for(int j=a; j>i; j--){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for(int j=1; j<i; j++){
+                System.out.print("**");;
+            }
+            if(i!=a){
+                System.out.println();
+            }
+        }
+        
+    }
+}
