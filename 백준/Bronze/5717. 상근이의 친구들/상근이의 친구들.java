016c6b95292f38d1c2fb627b8c14b9f26f97eb1d
김조을 <163392700+jo-eul@users.noreply.github.com>
2024-06-23 08:00:55 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        int a,b;
+        while (true){
+            a=scanner.nextInt();
+            b=scanner.nextInt();
+            if(a==0&&b==0){
+                break;
+            }
+            System.out.println(a+b);
+        }
+        scanner.close();
+    }
+}
