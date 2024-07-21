e64dd8f1783af26a53d828becf050c8473f34f35
김조을 <163392700+jo-eul@users.noreply.github.com>
2024-07-21 10:44:36 +0900

    [Bronze IV] Title: R2, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int r1=scanner.nextInt();
+        int s=scanner.nextInt();
+
+        System.out.println(s*2-r1);
+    }
+}
