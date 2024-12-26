d248fc59410e06bf501a6bc1acc30d1571df7e4b
김조을 <163392700+jo-eul@users.noreply.github.com>
2024-12-26 20:34:19 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.c
+++ 백준/Silver/9655. 돌 게임/돌 게임.c
@@ -1,34 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     int N;
     scanf("%d", &N); 
 
     
-    int dp[N + 1];
-
-    
-    dp[0] = 0; 
-    if (N >= 1) {
-        dp[1] = 1;  
+    if(N%2==0){
+        printf("CY");
     }
-
-    
-    for (int i = 2; i <= N; i++) {
-        dp[i] = 0; 
-        
-        if (i >= 1 && dp[i - 1] == 0) {
-            dp[i] = 1;
-        }
-        if (i >= 3 && dp[i - 3] == 0) {
-            dp[i] = 1;
-        }
-    }
-    
-    if (dp[N] == 1) {
-        printf("SK\n"); 
-    } else {
-        printf("CY\n"); 
+    else{
+        printf("SK");
     }
 
     return 0;
