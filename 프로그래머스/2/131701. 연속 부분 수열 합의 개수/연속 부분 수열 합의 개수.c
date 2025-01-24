0b34168d6d69d4e140ee548debe40b40e5e563b2
김조을 <163392700+jo-eul@users.noreply.github.com>
2025-01-24 18:01:55 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1174.48 ms, Memory: 4.61 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// elements_len은 배열 elements의 길이입니다.
+int solution(int elements[], size_t elements_len) {
+    int arr[elements_len];
+    int answer = 0;
+    return answer;
+}
