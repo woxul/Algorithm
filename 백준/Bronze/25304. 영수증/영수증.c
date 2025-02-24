8e8b84859de32964d27098892f91e97c3394ffde
조을 <163392700+woxul@users.noreply.github.com>
2025-02-24 12:41:58 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,n,a,b;
+    int sum=0;
+    scanf("%d",&x);
+    scanf("%d",&n);
+    for(int i=0; i<n; i++){
+        scanf("%d %d",&a,&b);
+        sum+=a*b;
+    }
+    if(x==sum)printf("Yes");
+    else printf("No");
+    return 0;
+}
