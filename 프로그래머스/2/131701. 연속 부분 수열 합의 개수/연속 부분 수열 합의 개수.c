0710fa6006dec1a2fb3339005233871fce36dc03
조을 <163392700+woxul@users.noreply.github.com>
2025-08-09 17:09:03 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3547.79 ms, Memory: 7.44 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.c
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.c
@@ -4,7 +4,23 @@
 
 // elements_len은 배열 elements의 길이입니다.
 int solution(int elements[], size_t elements_len) {
-    int arr[elements_len];
     int answer = 0;
+    int mem[1000010] = {0,};
+    for(int i=1;i<=elements_len;i++)
+    {
+        for(int j=0;j<elements_len;j++)
+        {
+            int sum=0;
+            for(int k=j;k<j+i;k++)
+            {
+                sum+=elements[k%elements_len];
+            }
+            if(mem[sum]==0)
+            {
+                answer++;
+                mem[sum]=1;
+            }
+        }
+    }
     return answer;
 }
